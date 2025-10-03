2d5b61d40ea8933817e4c23238a1f027f8663b19
Maxim Kuznetsov <mkuznetsov@voximplant.com>
2025-10-03 19:41:52 +0300

    Initial commit

---
+++ ios-sdk-v3-demo/AudioCallApp.swift
@@ -0,0 +1,14 @@
+//
+//  Copyright (c) 2011-2025, Voximplant, Inc. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct AudioCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios-sdk-v3-demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  Copyright (c) 2011-2025, Voximplant, Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

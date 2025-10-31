115d75fe22aa5785f3f02f00e47de7952b131079
Maxim Kuznetsov <mkuznetsov@voximplant.com>
2025-10-31 12:21:10 +0300

    New assets added

---
+++ ios-sdk-v3-demo/Resources/FontSet.swift
@@ -0,0 +1,14 @@
+//
+//  Copyright (c) 2011-2025, Voximplant, Inc. All rights reserved.
+//
+
+import SwiftUI
+
+enum FontSet {
+    static let body: Font = .system(size: 15, weight: .regular)
+    static let bodyBold: Font = .system(size: 15, weight: .bold)
+    static let bodyLarge: Font = .system(size: 17, weight: .regular)
+    static let bodyLargeBold: Font = .system(size: 17, weight: .bold)
+    static let subTitle: Font = .system(size: 21, weight: .regular)
+    static let largeTitle: Font = .system(size: 30, weight: .regular)
+}

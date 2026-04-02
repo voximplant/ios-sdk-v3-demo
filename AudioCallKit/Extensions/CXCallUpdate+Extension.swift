562a405dac9bad09fc076b5665ad34ceb78445b6
Maxim Kuznetsov <mkuznetsov@voximplant.com>
2026-04-02 11:13:11 +0300

    Disable hold supporting

--- AudioCallKit/Extensions/CXCallUpdate+Extension.swift
+++ AudioCallKit/Extensions/CXCallUpdate+Extension.swift
@@ -11,7 +11,7 @@ extension CXCallUpdate {
         // this setting beeing set to true leads to call kit option "add call" beeing active
         // so unless we drop call kit holding support, this button will be active
         // see https://stackoverflow.com/a/40510928/27624280 for details
-        self.supportsHolding = true
+        self.supportsHolding = false
         self.supportsGrouping = false
         self.supportsUngrouping = false
     }

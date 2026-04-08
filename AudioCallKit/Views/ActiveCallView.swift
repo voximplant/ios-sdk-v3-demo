0e748fe5a4dcbab99209f5e5f1ff2f795dcefe71
Maxim Kuznetsov <mkuznetsov@voximplant.com>
2026-04-08 12:47:51 +0300

    AudioDeviceError added

--- AudioCallKit/Views/ActiveCallView.swift
+++ AudioCallKit/Views/ActiveCallView.swift
@@ -7,6 +7,7 @@ import SwiftUI
 
 struct ActiveCallView: View {
     @EnvironmentObject private var callViewModel: CallViewModel
+    @StateObject private var audioDevicesViewModel = AudioDevicesViewModel()
 
     private enum Constants {
         static let padding: CGFloat = 32
@@ -55,11 +56,13 @@ struct ActiveCallView: View {
                 }
                 Spacer()
                 CallSettingsView()
+                    .environmentObject(audioDevicesViewModel)
                 CallAcceptView()
             }
             .padding(.top, Constants.topPadding)
             .padding(.bottom, Constants.padding)
             .padding(.horizontal, Constants.padding)
+            .toast(error: $audioDevicesViewModel.error)
         }
     }
 
@@ -137,7 +140,7 @@ struct CallAcceptView: View {
 
 struct CallSettingsView: View {
     @EnvironmentObject private var callViewModel: CallViewModel
-    @StateObject private var audioDevicesViewModel = AudioDevicesViewModel()
+    @EnvironmentObject private var audioDevicesViewModel: AudioDevicesViewModel
 
     private enum Constants {
         static let buttonSize: CGFloat = 40
